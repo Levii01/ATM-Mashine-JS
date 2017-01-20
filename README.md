23ce0a2e536f44c6b01155addb850becb59e4eeb
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-01-20 14:56:44 +0100

    Upload something file..

---
+++ README.md
@@ -0,0 +1,19 @@
+# ATM CashMashine
+
+Pin to account: '5555'.
+Operator password: 'secret123'.
+Password to unlock card: 'secret'.
+
+The ATM Machine
+
+Implement a cash machine using Object Oriented Design. The implementation should be based on the idea that a person inserts their card into the ATM, feed their PIN and follow the menu instructions to handle the following cases:
+
+  display current balance
+  withdraw money
+  charge the card with the money inserted into the ATM
+
+When they enter an invalid PIN, display an error message about this fact and disable their card so that it cannot be used until it is enabled by the bank.
+Display an error when a person tries to withdraw more money than they have in their bank account.
+Handle the case in which the person tries to withdraw more money than there is available in the ATM.
+Add a feature that saves all ATM operations into a log file.
+An ATM operator puts money into the ATM.
---
+++ atm.js
@@ -0,0 +1,217 @@
+var card = {
+  pin: 5,
+  authentication: false,
+  endabled: true,
+};
+var account = {
+  name: 'Miłosz',
+  surname: 'Sędziak',
+  balance: 500,
+  myCard: card,
+};
+var atm = {
+  money: 4000,
+  access: false,
+  password: 's',
+  secondPassword: 'secret',
+};
+var errors = {
+  cardDisabled: 'Card is disabled. Contact with operator to unlock card.',
+  blockCard: 'Card was disabled.',
+  wrongOperatorPassword: 'Wrong operator password',
+};
+
+function starter() {
+  timer();
+  firstChoice();
+}
+
+function firstChoice() {
+  document.getElementById('box1').innerHTML = '<h2>Select option:</h2>'
+  + '<button class="button" onclick="insertPin()"><span>Insert card </span></button> '
+  + '<br/><button class="button" onclick="pinOperator()"><span>Log in as operator </span></button>';
+  document.getElementById('box2').innerHTML = '';
+}
+
+
+// #
+// operator function!!!
+// #
+
+function pinOperator() {
+  document.getElementById('box2').innerHTML = '<h2>Insert password:</h2> '
+  + '<input type="text" id="passwordPlace" /><button class="button" onclick="testPasswordOperator()"><span>OK</span></button>';
+}
+
+function testPasswordOperator() {
+  insertedPassword = document.getElementById('passwordPlace').value;
+  // document.getElementById('box2').innerHTML = insertedPassword == atm.password;
+  if (insertedPassword == atm.password) {
+    operatorLogIn();
+    operatorOption();
+    document.getElementById('box2').innerHTML = '<h2>Correct password!.</h2>';
+  } else {
+    firstChoice();
+    document.getElementById('errors').innerHTML = errors.wrongOperatorPassword;
+  }
+}
+
+function operatorOption() {
+  document.getElementById('box1').innerHTML =
+  '<h2>ATM operator options:</h2> <button class="button" onclick="atmChargeMoney()"><span>Add money to ATM</span></button>'
+   + '<br/><button class="button" onclick="atmWithdrawMoney()"><span>Withdraw money from ATM</span></button>'
+   + '<br/><button class="button" onclick="showBalanceAtm()"><span>Show ATM balance</span></button>'
+   + '<br/><button class="button" onclick="endabledCard()"><span>Activate user card</span></button>'
+   + '<br/><button class="button" onclick="operatorLogOut()"><span>Log out</span></button>';
+}
+
+function atmChargeMoney() {
+  document.getElementById('box2').innerHTML = '<h2>Add money to ATM:</h2><br>'
+  + 'How much money you want to charge?<br>'
+  + '<input type="number" id="addMoneyAtm" /><button class="button" onclick="chargeAtm()"><span>OK</span></button>';
+}
+
+function chargeAtm() {
+  insertedMoneyAcc = parseInt(document.getElementById('addMoneyAccount').value);
+  account.balance += insertedMoneyAcc;
+  atm.money += insertedMoneyAcc;
+  document.getElementById('box2').innerHTML = '<h2>Charged account:</h2><br>'
+  + 'Amount: ' + insertedMoneyAcc + '$<br/> New balance: ' + account.balance + '$';
+}
+
+function userWithdrawMoney() {
+  document.getElementById('box2').innerHTML = '<h2>Withdraw money:</h2><br>'
+  + 'How much money you want to withdraw?<br>'
+  + '<input type="number" id="removeMoneyAccount" /><button class="button" onclick="withdrawAccount()"><span>OK</span></button>';
+}
+
+function withdrawAccount() {
+  insertedMoneyAcc2 = parseInt(document.getElementById('removeMoneyAccount').value);
+  account.balance -= insertedMoneyAcc2;
+  atm.money -= insertedMoneyAcc2;
+  document.getElementById('box2').innerHTML = '<h2>Charged account:</h2><br>'
+  + 'Amount: ' + insertedMoneyAcc2 + '$<br/> New balance: ' + account.balance + '$';
+}
+
+function showBalance() {
+  document.getElementById('box2').innerHTML = '<h2>Current balance:</h2><br><h3>' + account.balance + '$</h3>';
+}
+
+// #
+// account function!!!!!!
+// #
+function insertPin() {
+  document.getElementById('box2').innerHTML = '<h2>Insert pin card:</h2> '
+  + '<input type="number" id="pin" /><button class="button" onclick="testPinUser()"><span>OK</span></button>';
+}
+
+function testPinUser() {
+  insertedPin = parseInt(document.getElementById('pin').value);
+  if (card.endabled) {
+    pinAuthentication();
+  }else document.getElementById('errors').innerHTML = errors.cardDisabled;
+}
+
+function pinAuthentication() {
+  if (insertedPin == card.pin && card.endabled) {
+    logIn();
+    userOption();
+    document.getElementById('box2').innerHTML = '<h2>Correct pin number.</h2>';
+  } else {
+    firstChoice();
+    disabledCard();
+    document.getElementById('errors').innerHTML = errors.blockCard;
+  }
+}
+
+function userOption() {
+  document.getElementById('box1').innerHTML =
+  '<h2>Select option:</h2> <button class="button" onclick="userChargeMoney()"><span>Fund account</span></button>'
+   + '<br/><button class="button" onclick="userWithdrawMoney()"><span>Withdraw money</span></button>'
+   + '<br/><button class="button" onclick="showBalance()"><span>Show balance</span></button>'
+  //  + '<br/><button class="button" onclick="pinOperator()"><span>Owner information</span></button>'
+   + '<br/><button class="button" onclick="logOut()"><span>Log out</span></button>';
+}
+
+function userChargeMoney() {
+  document.getElementById('box2').innerHTML = '<h2>Fund account:</h2><br>'
+  + 'How much money you want to charge?<br>'
+  + '<input type="number" id="addMoneyAccount" /><button class="button" onclick="chargeAccount()"><span>OK</span></button>';
+}
+
+function chargeAccount() {
+  insertedMoneyAcc = parseInt(document.getElementById('addMoneyAccount').value);
+  account.balance += insertedMoneyAcc;
+  atm.money += insertedMoneyAcc;
+  document.getElementById('box2').innerHTML = '<h2>Charged account:</h2><br>'
+  + 'Amount: ' + insertedMoneyAcc + '$<br/> New balance: ' + account.balance + '$';
+}
+
+function userWithdrawMoney() {
+  document.getElementById('box2').innerHTML = '<h2>Withdraw money:</h2><br>'
+  + 'How much money you want to withdraw?<br>'
+  + '<input type="number" id="removeMoneyAccount" /><button class="button" onclick="withdrawAccount()"><span>OK</span></button>';
+}
+
+function withdrawAccount() {
+  insertedMoneyAcc2 = parseInt(document.getElementById('removeMoneyAccount').value);
+  account.balance -= insertedMoneyAcc2;
+  atm.money -= insertedMoneyAcc2;
+  document.getElementById('box2').innerHTML = '<h2>Charged account:</h2><br>'
+  + 'Amount: ' + insertedMoneyAcc2 + '$<br/> New balance: ' + account.balance + '$';
+}
+
+function showBalance() {
+  document.getElementById('box2').innerHTML = '<h2>Current balance:</h2><br><h3>' + account.balance + '$</h3>';
+}
+
+// selectors :)
+function logIn() {
+  card.authentication = true;
+}
+
+function logOut() {
+  card.authentication = false;
+  firstChoice();
+}
+
+function endabledCard() {
+  card.endabled = true;
+  document.getElementById('box2').innerHTML = '<h2>Status card:</2>'
+  + '<h3>Card has been activated.</h3>';
+}
+
+function disabledCard() {
+  card.endabled = false;
+}
+
+function operatorLogIn() {
+  atm.access = true;
+}
+
+function operatorLogOut() {
+  atm.access = false;
+  firstChoice();
+}
+
+function timer() {
+  var today = new Date();
+
+  var day = today.getDate();
+  var month = today.getMonth() + 1;
+  var year = today.getFullYear();
+
+  var hour = today.getHours();
+  if (hour < 10) hour = '0' + hour;
+  var minute = today.getMinutes();
+  if (minute < 10) minute = '0' + minute;
+  var second = today.getSeconds();
+  if (second < 10) second = '0' + second;
+
+  document.getElementById('clock').innerHTML = day + '.' + month + '.' + year +
+      ' | ' + hour + ':' + minute + ':' + second;
+
+  document.getElementById('status').innerHTML = 'Logged in: ' + card.authentication
+  + '<br/>Active card: ' + card.endabled + '<br/>Operator access: ' + atm.access + atm.money;
+  setTimeout('timer()', 1000);
+};
